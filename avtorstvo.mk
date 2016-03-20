858461cb6707451417547210b6e45ebfa0e9ae50
bine iljas <bine.iljas1@gmail.com>
2016-03-20 14:43:20 +0000

    Končna verzija spletnega stroboskopa

---
+++ avtorstvo.mk
@@ -0,0 +1,5 @@
+Študent z vpisno številko _63140086_ potrjujem, da sem __edini avtor__ oddane domače naloge.
+
+[Glavna](https://rawgit.com/binei/stroboskop/master/stroboskop.html)
+[Izgled](https://rawgit.com/binei/stroboskop/izgled/stroboskop.html)
+[Dinamika](https://rawgit.com/binei/stroboskop/dinamika/stroboskop.html)
